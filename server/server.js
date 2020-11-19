06e855df22ec07773a5d46661cb68bd3d5721f27
Mario <ortez3293@gmail.com>
2020-11-19 12:51:42 -0500

    Server codigo base

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('./config/config');
 
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser')
 
@@ -44,6 +45,13 @@ app.put('/usuario/:id', function(req, res) {
 app.delete('/usuario', function(req, res) {
   res.json('delete usuario')
 })
+
+
+mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+  if (err) throw err;
+
+  console.log('Base de datos online')
+});
 
 app.listen(process.env.PORT, () => {
   console.log('Escuchando puerto: ', process.env.PORT)
